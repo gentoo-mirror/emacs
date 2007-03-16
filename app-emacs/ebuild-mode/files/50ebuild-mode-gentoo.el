f59f609df9c61e495360e2254eef80cf5a05e1dc
Christian Faulhammer <opfer@gentoo.org>
2007-03-16 10:40:03 +0000

    straightened file extension handling a bit
    svn path=/emacs-overlay/; revision=50

--- app-emacs/ebuild-mode/files/50ebuild-mode-gentoo.el
+++ app-emacs/ebuild-mode/files/50ebuild-mode-gentoo.el
@@ -5,6 +5,5 @@
 (autoload 'ebuild-mode "ebuild-mode" "Major mode for Portage .ebuild and .eclass files." t)
 (autoload 'eselect-mode "ebuild-mode" "Major mode for Portage .eselect files." t)
 
-(add-to-list 'auto-mode-alist '("\\.ebuild\\'" . ebuild-mode))
-(add-to-list 'auto-mode-alist '("\\.eclass\\'" . ebuild-mode))
+(add-to-list 'auto-mode-alist '("\\.\\(ebuild\\|eclass\\)\\'" . ebuild-mode))
 (add-to-list 'auto-mode-alist '("\\.eselect\\'" . eselect-mode))
