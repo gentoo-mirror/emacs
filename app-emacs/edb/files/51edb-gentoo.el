642a175a48083dd9734125b970f334dae36a5af1
Ulrich Müller <ulm@gentoo.org>
2007-05-20 18:27:37 +0000

    Moved to Portage tree.
    svn path=/emacs-overlay/; revision=307

--- app-emacs/edb/files/51edb-gentoo.el
+++
@@ -1,7 +0,0 @@
-
-;; edb site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
-(let ((el "database") (blurb "Emacs Database"))
-  (autoload 'edb-EXPERIMENTAL-interact el blurb t)
-  (autoload 'db-find-file el blurb t))
