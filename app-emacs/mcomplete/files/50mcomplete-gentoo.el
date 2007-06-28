3edfdbd15df39b4bde60d8cde13163dc3b695274
Christian Faulhammer <opfer@gentoo.org>
2007-06-28 06:57:15 +0000

    initial import from bug 180170, by MATSUYAMA Tomohiro <matsuyama@ariel-networks.com>
    svn path=/emacs-overlay/; revision=506

---
+++ app-emacs/mcomplete/files/50mcomplete-gentoo.el
@@ -0,0 +1,14 @@
+
+;;: site-lisp configuration for mcomplete
+
+(add-to-list 'load-path "@SITELISP@")
+
+(autoload 'mcomplete-mode "mcomplete"
+  "Toggle minibuffer completion with prefix and substring matching."
+  t nil)
+(autoload 'turn-on-mcomplete-mode "mcomplete"
+  "Turn on minibuffer completion with prefix and substring matching."
+  t nil)
+(autoload 'turn-off-mcomplete-mode "mcomplete"
+  "Turn off minibuffer completion with prefix and substring matching."
+  t nil)
