b3eec9ab631ac987885f0c84d5946eee25e310b2
Ulrich Müller <ulm@gentoo.org>
2007-11-19 16:43:59 +0000

    Add ~amd64 keyword.
    svn path=/emacs-overlay/; revision=765

--- app-emacs/whine/files/50whine-gentoo.el
+++ app-emacs/whine/files/50whine-gentoo.el
@@ -3,7 +3,7 @@
 
 (add-to-list 'load-path "@SITELISP@")
 (autoload 'whinify "whine"
-  "Make certain global-mode keys whine.  The command `unwhinify' makes
-them normal again." t)
+  "Make certain global-mode keys whine.
+The command `unwhinify' makes them normal again." t)
 (autoload 'unwhinify "whine"
   "Hopefully, remove the whine-bindings on the whiny keys." t)
