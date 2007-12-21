66749218e1363dd64e1c72ce6c7b2a9dfcde3c64
Ulrich Müller <ulm@gentoo.org>
2007-12-21 14:50:25 +0000

    Moved to Portage tree.
    svn path=/emacs-overlay/; revision=887

--- app-emacs/whine/files/50whine-gentoo.el
+++
@@ -1,9 +0,0 @@
-
-;;; whine site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
-(autoload 'whinify "whine"
-  "Make certain global-mode keys whine.
-The command `unwhinify' makes them normal again." t)
-(autoload 'unwhinify "whine"
-  "Hopefully, remove the whine-bindings on the whiny keys." t)
