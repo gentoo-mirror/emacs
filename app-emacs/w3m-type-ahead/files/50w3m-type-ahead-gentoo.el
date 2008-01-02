4c8968867dc3fd410d58910d83171a568aad48aa
Ulrich Müller <ulm@gentoo.org>
2008-01-02 19:37:21 +0000

    New package app-emacs/w3m-type-ahead.
    svn path=/emacs-overlay/; revision=913

---
+++ app-emacs/w3m-type-ahead/files/50w3m-type-ahead-gentoo.el
@@ -0,0 +1,6 @@
+
+;;; w3m-type-ahead site-lisp configuration
+
+(add-to-list 'load-path "@SITELISP@")
+(autoload 'w3m-type-ahead-mode "w3m-type-ahead" nil t)
+(add-hook 'w3m-mode-hook 'w3m-type-ahead-mode)
