268d0333d6c3ba92df922bad8cb9e88bd1a299c5
Ulrich Müller <ulm@gentoo.org>
2008-01-24 08:36:34 +0000

    Break a long line.
    svn path=/emacs-overlay/; revision=934

--- app-emacs/browse-kill-ring/files/50browse-kill-ring-gentoo.el
+++ app-emacs/browse-kill-ring/files/50browse-kill-ring-gentoo.el
@@ -2,5 +2,5 @@
 ;;; browse-kill-ring site-lisp configuration
 
 (add-to-list 'load-path "@SITELISP@")
-
-(autoload 'browse-kill-ring "browse-kill-ring" "Display items in the `kill-ring' in another buffer." t nil)
+(autoload 'browse-kill-ring "browse-kill-ring"
+  "Display items in the `kill-ring' in another buffer." t nil)
