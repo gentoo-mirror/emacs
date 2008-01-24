4b3519d3bb91c8e9c1d66242e7a691ec0c3282cd
Ulrich Müller <ulm@gentoo.org>
2008-01-24 07:45:19 +0000

    Version bump. Licence is GPL-3 now. Fix site-init file.
    svn path=/emacs-overlay/; revision=928

--- app-emacs/color-moccur/files/50color-moccur-gentoo.el
+++ app-emacs/color-moccur/files/50color-moccur-gentoo.el
@@ -2,4 +2,14 @@
 ;;; color-moccur site-lisp configuration
 
 (add-to-list 'load-path "@SITELISP@")
-(require 'color-moccur)
+(mapc (function (lambda (x) (autoload x "color-moccur" nil t)))
+      '(moccur
+	dmoccur
+	dired-do-moccur
+	Buffer-menu-moccur
+	grep-buffers
+	search-buffers
+	occur-by-moccur
+	isearch-moccur
+	moccur-grep
+	moccur-grep-find))
