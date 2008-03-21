9acf64c44490a6680abe3278044633c6fc8d0c3c
Ulrich Müller <ulm@gentoo.org>
2008-03-21 19:13:20 +0000

    Move to Portage tree.
    svn path=/emacs-overlay/; revision=1019

--- app-emacs/browse-kill-ring/files/50browse-kill-ring-gentoo.el
+++
@@ -1,6 +0,0 @@
-
-;;; browse-kill-ring site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
-(autoload 'browse-kill-ring "browse-kill-ring"
-  "Display items in the `kill-ring' in another buffer." t nil)
