12eb35c5e787c8c4e89d0126ad6fbf1829761025
Ulrich Müller <ulm@gentoo.org>
2008-06-18 17:15:10 +0000

    Execute company-install-bundled-completions-rules after company-mode is loaded.
    svn path=/emacs-overlay/; revision=1094

--- app-emacs/company-mode/files/50company-mode-gentoo.el
+++ app-emacs/company-mode/files/50company-mode-gentoo.el
@@ -2,4 +2,9 @@
 ;;; company-mode site-lisp configuration
 
 (add-to-list 'load-path "@SITELISP@")
-(autoload 'company-mode "company-mode" "" t)
+(autoload 'company-mode "company-mode" nil t)
+(autoload 'company-install-bundled-completions-rules
+  "company-bundled-completions")
+
+(eval-after-load "company-mode"
+  '(company-install-bundled-completions-rules))
