4a4bd8b8a283b3b71409e0840b608043c5de303e
Ulrich Müller <ulm@gentoo.org>
2008-08-08 23:22:20 +0000

    Moved to Portage tree.
    svn path=/emacs-overlay/; revision=1126

--- app-emacs/bongo/files/50bongo-gentoo.el
+++
@@ -1,6 +0,0 @@
-
-;;; bongo site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
-(add-to-list 'image-load-path "@SITEETC@" t)
-(autoload 'bongo "bongo" "Start Bongo by switching to a Bongo buffer." t)
