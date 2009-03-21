5f25e1a9f9c739b2c08913174c9d07f5074df7d9
Ulrich Müller <ulm@gentoo.org>
2009-03-21 17:03:45 +0000

    Add path to checkstyle style file.
    svn path=/emacs-overlay/; revision=1335

--- app-emacs/jde/files/70jde-gentoo.el
+++ app-emacs/jde/files/70jde-gentoo.el
@@ -7,5 +7,6 @@
 (setq jde-java-directory "/usr/share/jde")
 (setq jde-bsh-jar-file "/usr/share/bsh/lib/bsh.jar")
 (setq jde-checkstyle-jar-file "/usr/share/checkstyle/lib/checkstyle.jar")
+(setq jde-checkstyle-style "/usr/share/checkstyle/checks/sun_checks.xml")
 (setq jde-html-directory "/usr/share/doc/@PF@/html")
 (setq bsh-html-directory "/usr/share/doc/@PF@/html")
