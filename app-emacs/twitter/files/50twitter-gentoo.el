785858489cae459ea6ecb07483be482f8b03c00c
Ulrich Müller <ulm@gentoo.org>
2009-07-23 19:48:55 +0000

    Initial import.
    svn path=/emacs-overlay/; revision=1429

---
+++ app-emacs/twitter/files/50twitter-gentoo.el
@@ -0,0 +1,4 @@
+(add-to-list 'load-path "@SITELISP@")
+(autoload 'twitter-get-friends-timeline "twitter" nil t)
+(autoload 'twitter-status-edit "twitter" nil t)
+(add-hook 'twitter-status-edit-mode-hook 'longlines-mode)
