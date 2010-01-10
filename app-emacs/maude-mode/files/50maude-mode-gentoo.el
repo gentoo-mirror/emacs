c51553728816f2bb9b198f0ecb71375d37fc2a0c
Ulrich Müller <ulm@gentoo.org>
2010-01-10 10:36:17 +0000

    Remove maude-mode.
    svn path=/emacs-overlay/; revision=1493

--- app-emacs/maude-mode/files/50maude-mode-gentoo.el
+++
@@ -1,7 +0,0 @@
-(add-to-list 'load-path "@SITELISP@")
-(add-to-list 'auto-mode-alist '("\\.maude\\'" . maude-mode))
-(autoload 'run-maude "maude-mode"
-  "Run an inferior Maude process, input and output via buffer *Maude*." t)
-(autoload 'run-full-maude "maude-mode" nil t)
-(autoload 'maude-mode "maude-mode" "Major mode for editing Maude files." t)
-(setq maude-command "maude")
