1e615bdbfb59e993ad08ae1680192e20879fe6cf
Ulrich Müller <ulm@gentoo.org>
2010-11-15 22:52:49 +0000

    Remove useless autoload.
    svn path=/emacs-overlay/; revision=1539

--- app-emacs/auto-complete/files/50auto-complete-gentoo.el
+++ app-emacs/auto-complete/files/50auto-complete-gentoo.el
@@ -1,6 +1,4 @@
 (add-to-list 'load-path "@SITELISP@")
-(autoload 'auto-complete "auto-complete"
-  "Start auto-completion at current point." t)
 (autoload 'auto-complete-mode "auto-complete"
   "AutoComplete mode" t)
 (setq ac-dictionary-directories "@SITEETC@/dict")
