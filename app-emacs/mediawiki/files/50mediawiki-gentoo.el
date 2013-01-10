bf0a5e5ef45549e4eeb1e74746f57ea953153db1
Ulrich Müller <ulm@gentoo.org>
2013-01-10 16:33:13 +0100

    Add missing quote to site file.
    Package-Manager: portage-2.1.11.38
    Manifest-Sign-Key: E7BE08CA7DC6EA25

--- app-emacs/mediawiki/files/50mediawiki-gentoo.el
+++ app-emacs/mediawiki/files/50mediawiki-gentoo.el
@@ -3,12 +3,8 @@
   "Open a wiki page specified by NAME from the mediawiki engine" t)
 (autoload 'mediawiki-site "mediawiki"
   "Set up mediawiki.el for a site." t)
-(autoload 'mediawiki-do-login "mediawiki"
-  "Use USERNAME and PASSWORD to log into the MediaWiki site and
-get a cookie." t)
 (eval-after-load "mediawiki"
-  (add-to-list 'mediawiki-site-alist
-	       '("Gentoo" "http://wiki.gentoo.org/"
-		 "username" "password" "Main Page")
-	       nil
-	       (lambda (a b) (equal (car a) (car b)))))
+  '(add-to-list 'mediawiki-site-alist
+		'("Gentoo" "http://wiki.gentoo.org/" "" "" "Main Page")
+		nil
+		(lambda (a b) (equal (car a) (car b)))))
