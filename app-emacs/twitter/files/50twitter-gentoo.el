eb0877df8c714605040686abe782b0cf44e79151
Ulrich Müller <ulm@gentoo.org>
2019-08-17 22:02:35 +0200

    app-emacs/twitter: Remove package.
    No longer working. Replacement is called "twitel" and (supposedly)
    available from http://www.busydoingnothing.co.uk/twitel/ but the git
    repository linked from there appears to be dead.
    
    Signed-off-by: Ulrich Müller <ulm@gentoo.org>

--- app-emacs/twitter/files/50twitter-gentoo.el
+++
@@ -1,4 +0,0 @@
-(add-to-list 'load-path "@SITELISP@")
-(autoload 'twitter-get-friends-timeline "twitter" nil t)
-(autoload 'twitter-status-edit "twitter" nil t)
-(add-hook 'twitter-status-edit-mode-hook 'longlines-mode)
